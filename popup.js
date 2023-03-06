0ac0e7b75954cc8f2a349e03732036db9f8eea81
dsplayerX <dumindu0043@gmail.com>
2023-03-06 23:47:48 +0530

    added 2 charts to display sentiment and sarcasm analysis results

--- popup.js
+++ popup.js
@@ -1,41 +1,10 @@
-const btn = document.getElementById("analyse");
-
-// function to draw a doughnut chart
-function drawDoughnutChart(data) {
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var chartData = {
-    labels: ["Positive", "Negative", "Neutral", "Sarcastic", "Non-Sarcastic"],
-    datasets: [
-      {
-        data: [
-          data["Positive Comments"],
-          data["Neutral Comments"],
-          data["Negative Comments"],
-          data["Sarcastic Comments"],
-          data["Nonsarcastic Comments"]
-        ],
-        backgroundColor: [
-          "#32CD32",
-          "#FF6347",
-          "#FFFF00",
-          "#800080",
-          "#4169E1",
-        ],
-      },
-    ],
-  };
-  var myChart = new Chart(ctx, {
-    type: "doughnut",
-    data: chartData,
-  });
-}
-
-btn.addEventListener("click", function () {
-  btn.disabled = true;
-  btn.innerHTML = "Analysing...";
+const analyseBtn = document.getElementById("analyse");
+analyseBtn.addEventListener("click", function () {
+  analyseBtn.disabled = true;
+  analyseBtn.innerHTML = "Analysing...";
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    var url = tabs[0].url;
-    var xhr = new XMLHttpRequest();
+    const url = tabs[0].url;
+    const xhr = new XMLHttpRequest();
     xhr.open(
       "GET",
       "http://127.0.0.1:5000/extensionresults?userinput=" + url,
@@ -43,25 +12,76 @@ btn.addEventListener("click", function () {
     );
     xhr.onload = function () {
       if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        var resultDiv = document.getElementById("results");
-        for (var prop in data) {
-          if (data.hasOwnProperty(prop)) {
-            var propDiv = document.createElement("div");
-            var nameSpan = document.createElement("span");
-            var valueSpan = document.createElement("span");
-            nameSpan.innerHTML = prop + ": ";
-            valueSpan.innerHTML = data[prop];
-            propDiv.appendChild(nameSpan);
-            propDiv.appendChild(valueSpan);
-            resultDiv.appendChild(propDiv);\
-            // draw the graph
-            drawDoughnutChart(data);
-          }
-        }
+        const data = JSON.parse(xhr.responseText);
+        const totalComments = data["Total Comments"];
+        const resultsDiv = document.getElementById("results");
+        resultsDiv.style.display = "block";
+        resultsDiv.innerHTML = `
+          <h4>Sentiment Analysis:</h4>
+          <div style="width: 100%">
+            <canvas id="sentimentChart"></canvas>
+          </div>
+          <h4>Sarcasm Analysis:</h4>
+          <div style="width: 100%">
+            <canvas id="sarcasmChart"></canvas>
+          </div>
+          <h5>Total comments: ${totalComments}</h5>
+        `;
+
+        // Create the sentiment chart
+        const sentimentChartCtx = document
+          .getElementById("sentimentChart")
+          .getContext("2d");
+        const sentimentChartData = {
+          labels: ["Positive", "Neutral", "Negative"],
+          datasets: [
+            {
+              label: "Sentiment Analysis",
+              data: [
+                data["Positive Comments"],
+                data["Neutral Comments"],
+                data["Negative Comments"],
+              ],
+              backgroundColor: ["#2ecc71", "#3498db", "#e74c3c"],
+            },
+          ],
+        };
+        const sentimentChartOptions = {
+          responsive: true,
+          maintainAspectRatio: false,
+        };
+        const sentimentChart = new Chart(sentimentChartCtx, {
+          type: "pie",
+          data: sentimentChartData,
+          options: sentimentChartOptions,
+        });
+
+        // Create the sarcasm chart
+        const sarcasmChartCtx = document
+          .getElementById("sarcasmChart")
+          .getContext("2d");
+        const sarcasmChartData = {
+          labels: ["Sarcastic", "Non-Sarcastic"],
+          datasets: [
+            {
+              label: "Sarcasm Analysis",
+              data: [data["Sarcastic Comments"], data["Nonsarcastic Comments"]],
+              backgroundColor: ["#f1c40f", "#9b59b6"],
+            },
+          ],
+        };
+        const sarcasmChartOptions = {
+          responsive: true,
+          maintainAspectRatio: false,
+        };
+        const sarcasmChart = new Chart(sarcasmChartCtx, {
+          type: "pie",
+          data: sarcasmChartData,
+          options: sarcasmChartOptions,
+        });
       }
-      btn.disabled = false;
-      btn.innerHTML = "Analyse";
+      analyseBtn.disabled = false;
+      analyseBtn.innerHTML = "Analyse";
     };
     xhr.send();
   });
